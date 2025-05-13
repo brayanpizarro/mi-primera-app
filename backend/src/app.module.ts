c638a1b4aa67ba54c9859b7047a8e3194551e403
brayanpizarro <brayan.pizarro@alumnos.ucn.cl>
2025-05-13 15:05:54 -0400

    se agrega carpeta auth

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 
 import { UsersModule } from './users/users.module';
 import { InventoryModule } from './inventory/inventory.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @Module({
@@ -21,6 +22,7 @@ import { InventoryModule } from './inventory/inventory.module';
     }),
     UsersModule,
     InventoryModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
