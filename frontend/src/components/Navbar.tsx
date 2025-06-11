52f54910ebb961904082f7ac392ea70ec9f77985
brayanpizarro <114868676+brayanpizarro@users.noreply.github.com>
2025-06-11 10:30:27 -0400

    navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import { useNavigate, Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { UserContext } from '../context/UserContext';
+import './Navbar.css';
+import '../pages/Register.css';
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  const { user, logout } = useContext(UserContext);
+
+  if (!user) {
+    return null; // No mostrar el navbar si no hay usuario autenticado
+  }
+
+  const handleLogout = () => {
+    logout();
+    navigate('/login');
+  };
+  
+  const isAdmin = user?.role === 'admin';
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <span>StockMind UCN</span>
+      </div>
+      <div className="navbar-menu">        {isAdmin ? (
+          <>
+            <Link to="/inventory" className="navbar-item">
+                Ver Inventario
+            </Link>
+            <Link to="/register" className="navbar-item">
+            
+              Registrar Usuario
+            
+            </Link>
+          </>
+        ) : (
+          <Link to="/inventoryUser" className="navbar-item">
+            Ver Inventario
+          </Link>
+        )}
+      </div>
+      <div className="navbar-end">
+        <div className="navbar-user">
+          <span className="user-name">{user?.name}</span>
+          <span className="user-role">{user?.role === 'admin' ? 'Administrador' : 'Usuario'}</span>
+        </div>
+        <button className="logout-button" onClick={handleLogout}>
+          Cerrar Sesión
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
